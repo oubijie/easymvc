e37b783fcde885151c01c726276fa439e09a0f97
oubijie <oubijie@139.com>
2019-07-19 16:11:48 +0800

    增加方法调用，访问/user/add能执行UserController的add方法

--- src/com/veryoo/controller/UserController.java
+++ src/com/veryoo/controller/UserController.java
@@ -5,4 +5,20 @@ public class UserController extends BaseController{
 	public String execute(){
 		return "/WEB-INF/jsp/user.jsp";
 	}
+	
+	public String list(){
+		return "/WEB-INF/jsp/user/list.jsp";
+	}
+	
+	public String add(){
+		return "/WEB-INF/jsp/user/add.jsp";
+	}
+	
+	public String edit(){
+		return "/WEB-INF/jsp/user/edit.jsp";
+	}
+	
+	public String save(){
+		return "/WEB-INF/jsp/user/list.jsp";
+	}
 }
--- src/com/veryoo/servlet/EasyDefaultServlet.java
+++ src/com/veryoo/servlet/EasyDefaultServlet.java
@@ -2,6 +2,8 @@ package com.veryoo.servlet;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.util.IllegalFormatException;
+import java.util.StringTokenizer;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -19,50 +21,78 @@ public class EasyDefaultServlet extends HttpServlet{
 	protected void service(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		
-		String result = "404.jsp";
+		String result = "/404.jsp";
 		
-		String uri = req.getRequestURI();
-		String controllerName = getControllerName(uri);
-		System.out.println("请求的URL：" + uri);
-		System.out.println("处理的Controller：" + controllerName);
+		String controllerName = "";
+		String methodName = "";
 		
 		try {
+			
+			String uri = req.getRequestURI();
+			controllerName = getControllerName(uri);
+			System.out.println("请求的URL：" + uri);
+			System.out.println("处理的Controller：" + controllerName);
+			methodName = getMethodName(uri);
+			System.out.println("处理的Method：" + methodName);
+		
 			Class cls = Class.forName(controllerName);
 			Object controller = cls.newInstance();
 			//TODO 这里写死了拿父类的setRequest和setResponse方法，所以Controller只能一层继承
 			cls.getSuperclass().getMethod("setRequest", HttpServletRequest.class).invoke(controller, req);
 			cls.getSuperclass().getMethod("setResponse", HttpServletResponse.class).invoke(controller, resp);
-			result = (String)cls.getMethod("execute").invoke(controller);
+			
+			//调用Controller的处理方法
+			result = (String)cls.getMethod(methodName).invoke(controller);
 		} catch (ClassNotFoundException e) {
 			System.err.println("找不到处理的类：" + controllerName);
 			e.printStackTrace();
-			result = "500.jsp";
+			result = "/500.jsp";
 		} catch (InstantiationException | IllegalAccessException e) {
 			System.err.println("实例化类出错：" + controllerName);
 			e.printStackTrace();
-			result = "500.jsp";
+			result = "/500.jsp";
 		} catch (IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException e) {
 			System.err.println("执行Controller的execute方法出错：" + controllerName);
 			e.printStackTrace();
-			result = "500.jsp";
+			result = "/500.jsp";
 		} 
-		
-//		if(uri.equals("/home")){
-//			HomeController controller = new HomeController();
-//			result = controller.execute();
-//		}else if(uri.equals("/user")){
-//			UserController controller = new UserController();
-//			result = controller.execute();
-//		}
 		
 		req.getRequestDispatcher(result).forward(req, resp);
 	}
 	
+	
+
 	//根据请求的URL地址返回处理请求的Controller全称
 	private String getControllerName(String uri){
 		String name = uri.substring(1);
-		name = name.substring(0, 1).toUpperCase() + name.substring(1);
+		//GET请求的参数带在url上，需要先过滤掉
+		if(uri.indexOf("?") != -1){
+			name = uri.substring(0, uri.indexOf("?"));
+		}
+		int splitIndex = name.indexOf("/")==-1 ? name.length() : name.indexOf("/");  //找到第二个/的下标
+		name = name.substring(0, 1).toUpperCase() + name.substring(1, splitIndex);
 		name = "com.veryoo.controller." + name + "Controller";  //TODO 包名应从配置获取，或者根据当前servlet的包名获取
 		return name;
+	}
+	
+	//根据请求的URL地址返回处理请求的方法
+	private String getMethodName(String uri) {
+		String methodName = "execute";
+		//GET请求的参数带在url上，需要先过滤掉
+		if(uri.indexOf("?") != -1){
+			uri = uri.substring(0, uri.indexOf("?"));
+		}
+		StringTokenizer st = new StringTokenizer(uri, "/");
+		if(st.countTokens() <= 0 || st.countTokens() > 2){
+			throw new IllegalArgumentException("URL不正确");
+		}
+		String name = uri.substring(1);
+		if(name.indexOf("/") != -1){
+			methodName = name.substring(name.indexOf("/")+1).replace("/", "");
+		}
+		if(methodName.equals("")){
+			methodName = "execute";
+		}
+		return methodName;
 	}
 }
