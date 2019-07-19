880e7f71225f5ee27d0bd461425502c039539be1
oubijie <oubijie@139.com>
2019-07-19 14:31:38 +0800

    项目初始化

---
+++ src/com/veryoo/servlet/EasyDefaultServlet.java
@@ -0,0 +1,9 @@
+package com.veryoo.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(value={"/"})
+public class EasyDefaultServlet extends HttpServlet{
+
+}
